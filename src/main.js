511613c940fec814d07089fe04b099b1be6dcd15
jihoon <jihoon6372@hanmail.net>
2018-10-17 04:17:15 +0900

    불필요한 코드 제거

--- src/main.js
+++ src/main.js
@@ -14,15 +14,6 @@ require('@/assets/css/tower.css');
 require('@/assets/css/tower_m.css');
 require('@/assets/css/carousel_core.css');
 
-/* js */
-// require('@/assets/js/mouse_wheel.js');
-// require('@/assets/js/carousel_core.js');
-// require('@/assets/js/main_carousel.js');
-// require('@/assets/js/sc_player.js');
-// require('@/assets/js/preview.js');
-// require('@/assets/js/common.js');
-// require('@/assets/js/taginput.js');
-
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
