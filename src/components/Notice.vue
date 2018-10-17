21bd2b32a1d80cac9152f40a2d59c9e72390bcf8
jihoon <jihoon6372@hanmail.net>
2018-10-17 20:57:49 +0900

    공지사항 컴포넌트 추가

---
+++ src/components/Notice.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <div class="sub_title" :style="notice_header">
+            <div class="title_area">
+                <h2>NOTICE</h2>
+                <h1>NOTICE</h1>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['set_se_pre_con'],
+    data() {
+        return {
+            notice_header: {
+                'background-image': 'url('+require('@/assets/img/sub_main/sub_main01.jpg')+')'
+            }
+        }
+    },
+    mounted() {
+        this.set_se_pre_con(false);
+    }    
+}
+</script>
